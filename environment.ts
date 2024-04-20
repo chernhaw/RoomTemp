ffe6ef752676c40637220ab84cbe16f4ff2fe56c
chernhaw <38425578+chernhaw@users.noreply.github.com>
2024-04-20 18:30:41 +0800

    Update environment.ts

--- environment.ts
+++ environment.ts
@@ -1,10 +1,10 @@
 export const environment ={
-    apiKey: "AIzaSyAXKq3Zq0dDnk3NdQBRQ7LEG_pDeUkFPXM",
+    apiKey: "AM",
     authDomain: "myroomtemp-c7758.firebaseapp.com",
     databaseURL: "https://myroomtemp-c7758.firebaseio.com",
     projectId: "myroomtemp-c7758",
     storageBucket: "myroomtemp-c7758.appspot.com",
-    messagingSenderId: "668602182554",
-    appId: "1:668602182554:web:9a2559a40504008fe7e823"
+    messagingSenderId: "4",
+    appId: "1:"
 
 }
