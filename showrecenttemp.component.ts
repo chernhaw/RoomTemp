a10021e8e52e4457c6d2d3248310af0d7e6def1b
chernhaw <38425578+chernhaw@users.noreply.github.com>
2024-04-27 20:16:20 +0800

    Add files via upload
    Convert to the recent data to JSON to be used in ngFor

--- showrecenttemp.component.ts
+++ showrecenttemp.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase, } from '@angular/fire/compat/database';
 import { Observable } from 'rxjs';
+import { TempHumidity } from '../temp-humidity';
 
 @Component({
   selector: 'app-showrecenttemp',
@@ -13,27 +14,77 @@ export class ShowrecenttempComponent  {
 
   recentData: any;
   recentDataStr =''
-  recentDataArray=[]
+  recentDataArray: any =[];
+  recentTemp:any = [];
+
+  
+  
+  //humiditytemp
+  tempHumidityPt =''
   
   constructor(private db: AngularFireDatabase) {
     
   }
 
   ngOnInit(): void {
-    this.db.object('sensor/recent').valueChanges()
-      .subscribe((snapshot) => {
-        this.recentData = snapshot;
+
+    this.db.object('sensor/recent').valueChanges().pipe(
+      
+    ).subscribe((data) => {
+      // Handle the data here
+      this.recentData = data;
         this.recentDataStr =this.recentData.json
       //  this.recentDataStr = JSON.stringify(this.recentData);
         console.log(this.recentDataStr);
         var recentDataArray=this.recentDataStr.split('|')
-        console.log(recentDataArray);
+   
+        
+        console.log(recentDataArray)
+        // for (var i = 0; i < recentDataArray.length; i++) {
+        var date =''
+        var temperature =''
+        var humidity
+        for (let value of recentDataArray) {
+            console.log(value);
+            
+            var dateTemp = value.split("Temp")
+            console.log(dateTemp)
 
-        const match = recentDataArray[0].match(/(\d+\.\d+)/);
-        console.log(match);
+            date=dateTemp[0]
+            temperature=dateTemp[1]
 
-        // Output the data
-      });
+            
+            console.log("Date "+date)
+            var tempHumid = value.split("H")
+
+            console.log(tempHumid[1])
+            temperature=tempHumid[1]
+            humidity=tempHumid[1]
+
+            var tempHumidityJSON = {
+              "date":date,
+              "temperature":temperature,
+              "humidity":humidity
+            }
+
+            console.log("Date : "+date + " Temperature : "+temperature +" Humidity : "+humidity)
+            this.recentTemp.push(tempHumidityJSON)
+          }
+         
+          
+
+          console.log(this.recentTemp)
+
+        
+    }
+            
+       
+    );
+    
+      
+        
+        
+      
 
 
       
