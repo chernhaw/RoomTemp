a463879ffa14e3274e95f0302853086426c2fe73
chernhaw <38425578+chernhaw@users.noreply.github.com>
2024-04-28 15:21:29 +0800

    Add files via upload
    added color to background

--- recenttempcard.component.ts
+++ recenttempcard.component.ts
@@ -1,6 +1,6 @@
 import { Component,Input } from '@angular/core';
 import { TempHumidity } from '../temp-humidity';
-import { ShowrecenttempComponent } from '../showrecenttemp/showrecenttemp.component';
+
 
 @Component({
   selector: 'app-recenttempcard',
@@ -9,7 +9,50 @@ import { ShowrecenttempComponent } from '../showrecenttemp/showrecenttemp.compon
 })
 export class RecenttempcardComponent {
 
-  @Input() humiditytemp!:String[] 
+  backgroundColor = 'cyan';
 
-  
+  ngOnInit(): void {
+
+    console.log(parseFloat(this.tempHumidity.temperature))
+
+    if(parseFloat(this.tempHumidity.temperature)==29.0 ){
+
+      this.backgroundColor = '#ffe0cc';
+
+
+    } else
+    
+    if (parseFloat(this.tempHumidity.temperature)==30.0){
+
+      this.backgroundColor = '#ffc299';
+
+
+    }
+    
+    else
+    
+    if (parseFloat(this.tempHumidity.temperature)==31.0){
+
+      this.backgroundColor = '#ffa366';
+
+
+    } else
+    if(parseInt(this.tempHumidity.temperature)==32.0){
+      console.log("set to 32 color" )
+      this.backgroundColor = '#ff944d';
+    } else if (parseInt(this.tempHumidity.temperature)==33){
+      console.log("set to 33 color" )
+      this.backgroundColor = '#ff8533';
+    } else if (parseFloat(this.tempHumidity.temperature)==34.0 ){
+      this.backgroundColor = '#ff751a';
+    } else if (parseFloat(this.tempHumidity.temperature)==35.0 ){
+      this.backgroundColor = '#ff6600';
+    } else if (parseFloat(this.tempHumidity.temperature)>35.0 ){
+      this.backgroundColor = '#e65c00';
+    }
+
+  }
+
+  @Input() tempHumidity!:TempHumidity;
+
 }
